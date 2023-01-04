15c2abf21588e8d8110a38560f76de78ca85e72f
Sunny Jaiswal <sunnyjaiswal@Sunnys-MacBook-Air.local>
2023-01-05 02:14:23 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: const Text(" Welcome to My App "),
+          ),
+        ),
+      ),
+    );
+  }
+}
