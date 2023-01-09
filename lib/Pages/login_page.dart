0d375b6ad4407dcbcf3e17bcb6012166bb4d3a3b
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2023-01-10 01:02:35 +0530

    day 4 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -14,6 +14,7 @@ class LoginPage extends StatelessWidget {
             color: Colors.black,
             fontWeight: FontWeight.bold,
           ),
+          textScaleFactor: 1.5,
         ),
       ),
     );
